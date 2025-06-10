b71d4ad53ef45d3a812beadd3b7ffd8ed59823a3
Debugger022 <debugger022@gmail.com>
2025-06-10 18:38:49 +0530

    feat: update interface

--- contracts/ERC4626/Interfaces/IComptroller.sol
+++ contracts/ERC4626/Interfaces/IComptroller.sol
@@ -10,9 +10,15 @@ import { RewardsDistributor } from "@venusprotocol/isolated-pools/contracts/Rewa
  * @notice Combined interface for the `Comptroller` contract, including both core and view functions.
  */
 interface IComptroller {
+    function claimVenus(address) external;
+
     function actionPaused(address market, Action action) external view returns (bool);
 
     function getRewardDistributors() external view returns (RewardsDistributor[] memory);
 
     function supplyCaps(address) external view returns (uint256);
+
+    function markets(address) external view returns (bool, uint);
+
+    function getXVSAddress() external view returns (address);
 }
---
+++ contracts/ERC4626/Interfaces/VTokenInterface.sol
@@ -0,0 +1,27 @@
+pragma solidity ^0.8.25;
+
+import { IComptroller } from "./IComptroller.sol";
+
+abstract contract VTokenInterface {
+    function mint(uint mintAmount) external virtual returns (uint);
+
+    function redeem(uint redeemTokens) external virtual returns (uint);
+
+    function redeemUnderlying(uint redeemAmount) external virtual returns (uint);
+
+    function balanceOf(address owner) external view virtual returns (uint);
+
+    function comptroller() external view virtual returns (IComptroller);
+
+    function totalSupply() external view virtual returns (uint);
+
+    function underlying() external view virtual returns (address);
+
+    function getCash() external view virtual returns (uint);
+
+    function exchangeRateStored() public view virtual returns (uint);
+
+    function accrueInterest() public view virtual returns (uint);
+
+    function totalReserves() public view virtual returns (uint);
+}
